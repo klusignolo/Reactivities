c854f42e6898d82d0dcba58ff6111b66c389906a
Kevin Lusignolo <kevin.lusignolo@hcss.com>
2020-04-01 11:32:52 -0500

    End of Section 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,25 +1,21 @@
-import React, { SyntheticEvent } from 'react'
+import React, { useContext } from 'react'
 import { Item, Button, Segment, Label } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
+import { observer } from 'mobx-react-lite'
+import ActivityStore from '../../../app/stores/activityStore'
 
-interface IProps {
-    activities: IActivity[];    
-    selectActivity: (id: string) => void;
-    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-    submitting: boolean;
-    target: string;
-}
-
-export const ActivityList: React.FC<IProps> = ({
-    activities,
-    selectActivity,
-    deleteActivity,
-    submitting,
-    target}) => {
+export const ActivityList: React.FC = () => {
+    const activityStore = useContext(ActivityStore);
+    const {
+        activitiesByDate,
+        selectActivity,
+        deleteActivity,
+        submitting,
+        target
+    } = activityStore;
     return (
         <Segment clearing>
           <Item.Group divided>
-              {activities.map(activity => (
+              {activitiesByDate.map(activity => (
                 <Item key={activity.id}>
                     <Item.Content>
                         <Item.Header as='a'>{activity.title}</Item.Header>
@@ -36,11 +32,11 @@ export const ActivityList: React.FC<IProps> = ({
                                 color='blue' />
                             <Button 
                                 name={activity.id}
+                                loading={target === activity.id && submitting}
                                 onClick={(e) => deleteActivity(e, activity.id)} 
                                 floated='right' 
                                 content='Delete' 
                                 color='red'
-                                loading={target === activity.id && submitting}
                             />
                             <Label basic content={activity.category} />
                         </Item.Extra>
@@ -51,3 +47,5 @@ export const ActivityList: React.FC<IProps> = ({
         </Segment>        
     )
 }
+
+export default observer(ActivityList);
